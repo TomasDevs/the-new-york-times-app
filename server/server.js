49368e57b63e92de3564c18baf7de2526b665d38
Tomáš Štveráček <tomas.stveracek@gmail.com>
2024-08-30 15:35:53 +0200

    Modify server.js

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ app.get("/api/articles", async (req, res) => {
   }
 });
 
-// Start the server on the specified port
+// Start the server on the specified port and log a message
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
